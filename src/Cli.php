97815684550648dbba0fc490639d5d950fac307f
Vladimir Yashnikov <yashnikov@yashnikov.com>
2023-01-09 20:33:48 +0300

    rename function

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting() {
+function greetPerson() {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
