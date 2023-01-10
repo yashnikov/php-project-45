4b9f5ac25e46f98c46e78e52d18e8140bf5e2a31
Vladimir Yashnikov <yashnikov@yashnikov.com>
2023-01-10 23:01:44 +0300

    fix linter error

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greetPerson() {
+function greetPerson()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
